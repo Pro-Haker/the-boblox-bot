dfc530463617f53822462c8ab5f01e3a90d902d8
Pro-Haker <julek.wilczek@gmail.com>
2024-12-07 18:08:21 +0100

    arg test works

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 import os
 import discord
+from discord import app_commands
 from discord.ext import commands
 from dotenv import load_dotenv
 
@@ -14,6 +15,11 @@ async def on_ready():
 @client.tree.command(name="test", description="Test the bot")
 async def test(interaction: discord.Interaction):
     await interaction.response.send_message("HOLY SHIT IT FUCKING WORKS!!!")
+
+@client.tree.command(name="test2", description="Test the bot with arguments")
+@app_commands.describe(arg="Test argument")
+async def test2(interaction: discord.Interaction, arg: str):
+    await interaction.response.send_message(f"The argument you typed was \"{arg}\"")
 
 load_dotenv()
 client.run(os.getenv("BOT_TOKEN"))
